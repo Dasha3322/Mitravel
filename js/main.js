c85dabb365a009fb66ba35c3156ca2908f6cf455
Dasha Babanina <166533809+Dasha3322@users.noreply.github.com>
2024-06-29 02:28:11 +0300

    Mitravel

--- js/main.js
+++ js/main.js
@@ -5,12 +5,7 @@ $(".burger").click(function(){
 })
 let myImageSlider = new Swiper('.image-slider',{
   shortSwipes: false,
-  slidesPerView: 1,
-  spaceBetween: 30,
   loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'},
     } 
 );
 let myTextSlider = new Swiper('.text-slider',{
@@ -26,3 +21,32 @@ let myTextSlider = new Swiper('.text-slider',{
 
 myImageSlider.controller.control = myTextSlider;
 myTextSlider.controller.control = myImageSlider;
+
+
+
+new Swiper('.featured-videos-swiper',{
+  direction:'horizontal',
+  slidesPerView: 1.8,
+  // loop: true,
+  centeredSlides: true,
+  spaceBetween: 30,
+  autoplay: true,
+  
+  breakpoints: {
+    320:{
+    slidesPerView: 1},
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      spaceBetween: 25
+    },
+    // when window width is >= 1200px
+    1201: {
+      spaceBetween: 30
+    },
+},
+});
